0e4c13e88d25b71f01eaa5eb0796ea9b0dbc458c
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-05-15 15:40:06 +0800

    add recorder hint

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def clear():
         os.remove("input.wav")
 
 def app():
-    st.write("Click start button to record audio.")
+    st.header("🎙️ Audio Recorder")
     def in_recorder_factory() -> MediaRecorder:
         return MediaRecorder(
             "input.wav", format="wav"
         )
 
-    webrtc_streamer(
+    webrtc_ctx = webrtc_streamer(
         key="loopback",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
@@ -28,11 +28,23 @@ def app():
         sendback_audio=False,
         in_recorder_factory=in_recorder_factory,
     )
+    status_indicator = st.empty()
     try:
         st.audio('input.wav', format="audio/wav")
         clear() # remove audio file
     except:
-        st.write("No record media.")
+        status_indicator.write('No record media. Click start button to record audio.')
+    
+    if webrtc_ctx.state.playing:
+        # center layout
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write(' ')
+        with col2:
+            st.image('https://jp.easeus.com/images_2019/multimedia/recexperts/feature-2.gif')
+            status_indicator.write('Recording...')
+        with col3:
+            st.write(' ')
 
 if __name__ == "__main__":
     app()
